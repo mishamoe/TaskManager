c2718ea44c89e8cb0f3e0941930027021859ade8
Mihail Moyseyenko <Misha@Mac-mini-Mihail.local>
2015-04-25 15:44:11 +0300

    Initial Commit

---
+++ TaskManager/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  TaskManager
+//
+//  Created by Mihail Moyseyenko on 25.04.15.
+//  Copyright (c) 2015 Mihail Moyseyenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ TaskManager/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  TaskManager
+//
+//  Created by Mihail Moyseyenko on 25.04.15.
+//  Copyright (c) 2015 Mihail Moyseyenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
